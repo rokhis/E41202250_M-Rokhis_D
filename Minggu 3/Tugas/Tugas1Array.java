e91a9ddcb7359d456f2b35f7babbce62b16c7a85
Rokhis <75311518+rokhis@users.noreply.github.com>
2021-03-23 06:24:17 +0700

    Add files via upload

---
+++ Minggu 3/Tugas/Tugas1Array.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas1array;
+
+/**
+ *
+ * @author Rokhis
+ */
+public class Tugas1Array {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String [] nama = {"Muhammad", "Rokhis"};
+        
+        for (int r=0; r<nama.length; r++){
+            System.out.print(nama[r]);
+            System.out.print(" ");
+    }
+    
+    }}
