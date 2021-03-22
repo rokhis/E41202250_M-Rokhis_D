ab3fe4fa975ea2cc8f7078b256061b5c6dada752
Rokhis <75311518+rokhis@users.noreply.github.com>
2021-03-23 06:41:50 +0700

    Add files via upload

---
+++ Minggu 3/Tugas/Tugas2Array.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas2array;
+
+/**
+ *
+ * @author Rokhis
+ */
+public class Tugas2Array {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int [] angka = {145,232,332,123,165,543};
+        
+        for (int i=0; i<angka.length; i++){
+            System.out.println(angka[i]);
+    }
+    
+    }}
